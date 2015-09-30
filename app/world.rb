21bf6ca6c7ff6799a9253853d84306ef867e5427
Albin Svensson <albin.svensson@trialbee.com>
2015-09-30 19:32:10 +0200

    Refactor for readability and performance

--- app/world.rb
+++ app/world.rb
@@ -1,16 +1,16 @@
 class World
-  attr_reader :width, :height, :obstacles 
+  attr_reader :x_range, :y_range, :width, :height, :obstacles 
   def initialize(width, height, obstacles = [])
-    @width = width 
-    @height = height
+    @x_range   = 0..width
+    @y_range   = 0..height
+    @width     = width
+    @height    = height
     @obstacles = obstacles
   end
 
   def allowed_position?(position)
-    return false if position.x < 0
-    return false if position.x > width
-    return false if position.y < 0
-    return false if position.y > height
+    return false unless x_range.cover?(position.x)
+    return false unless y_range.cover?(position.y)
     true
   end
 end
