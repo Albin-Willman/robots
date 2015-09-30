eb8dece4fd5b3539e02b9b076595842ad1e16e08
Albin Svensson <albin.svensson@trialbee.com>
2015-09-30 19:56:08 +0200

    Added check accessibility to robot before move

--- app/robot.rb
+++ app/robot.rb
@@ -1,9 +1,11 @@
 class Robot
   DIRECTIONS = ['N', 'E', 'S', 'W'].freeze
   attr_accessor :position, :direction
-  def initialize(position, direction)
+  attr_reader :world
+  def initialize(position, direction, world)
     @position  = position
     @direction = direction
+    @world     = world
   end
 
   def turn_left!
@@ -25,19 +27,25 @@ class Robot
   private
 
   def move!(directionality)
-    case direction
-    when 'N'
-      @position = Position.new(position.x, position.y - directionality)
-    when 'S'
-      @position = Position.new(position.x, position.y + directionality)
-    when 'W'
-      @position = Position.new(position.x - directionality, position.y)
-    when 'E'
-      @position = Position.new(position.x + directionality, position.y)
-    end
+    new_position = next_position(directionality)
+    return false unless world.allowed_position?(new_position)
+    @position = new_position
   end
 
   def turn!(change)
     @direction = DIRECTIONS[(DIRECTIONS.index(direction) + change) % DIRECTIONS.length]
+  end
+
+  def next_position(directionality)
+    case direction
+    when 'N'
+      Position.new(position.x, position.y - directionality)
+    when 'S'
+      Position.new(position.x, position.y + directionality)
+    when 'W'
+      Position.new(position.x - directionality, position.y)
+    when 'E'
+      Position.new(position.x + directionality, position.y)
+    end
   end
 end
--- tests/robot_test.rb
+++ tests/robot_test.rb
@@ -1,6 +1,7 @@
 require "minitest/autorun"
 require "./app/robot"
 require "./app/position"
+require "./app/world"
 
 class TestRobot < Minitest::Test
   def test_basic_position
@@ -105,7 +106,68 @@ class TestRobot < Minitest::Test
     assert_equal(4, r.position.y)
   end
 
-  def new_robot(pos: Position.new(4, 4), direction: 'N')
-    Robot.new(pos, direction)
+
+  def test_move_outside_world_north
+    r = new_robot(pos: Position.new(4, 0))
+    r.forward!
+    assert_equal(4, r.position.x)
+    assert_equal(0, r.position.y)
+  end
+
+  def test_move_outside_world_south
+    r = new_robot(pos: Position.new(4, 100), direction: 'S')
+    r.forward!
+    assert_equal(4, r.position.x)
+    assert_equal(100, r.position.y)
+  end
+
+  def test_move_outside_world_west
+    r = new_robot(pos: Position.new(0, 40), direction: 'W')
+    r.forward!
+    assert_equal(0, r.position.x)
+    assert_equal(40, r.position.y)
+  end
+
+  def test_move_outside_world_east
+    r = new_robot(pos: Position.new(100, 40), direction: 'E')
+    r.forward!
+    assert_equal(100, r.position.x)
+    assert_equal(40, r.position.y)
+  end
+
+  def test_move_blocked_by_obstacle_north
+    r = new_robot(pos: Position.new(30, 31))
+    r.forward!
+    assert_equal(30, r.position.x)
+    assert_equal(31, r.position.y)
+  end
+
+  def test_move_blocked_by_obstacle_south
+    r = new_robot(pos: Position.new(30, 29), direction: 'S')
+    r.forward!
+    assert_equal(30, r.position.x)
+    assert_equal(29, r.position.y)
+  end
+
+  def test_move_blocked_by_obstacle_west
+    r = new_robot(pos: Position.new(31, 30), direction: 'W')
+    r.forward!
+    assert_equal(31, r.position.x)
+    assert_equal(30, r.position.y)
+  end
+
+  def test_move_blocked_by_obstacle_south
+    r = new_robot(pos: Position.new(29, 30), direction: 'E')
+    r.forward!
+    assert_equal(29, r.position.x)
+    assert_equal(30, r.position.y)
+  end
+
+  def new_robot(pos: Position.new(4, 4), direction: 'N', world: create_world)
+    Robot.new(pos, direction, world)
+  end
+
+  def create_world(width: 100, height: 100, obstacles: [Position.new(30, 30)])
+    World.new(width, height, obstacles)
   end
 end
