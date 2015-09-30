c368b95c39ab122d674cad498c03cad5fffdee13
Albin Svensson <albin.svensson@trialbee.com>
2015-09-30 18:40:44 +0200

    Ability to turn left

--- app/robot.rb
+++ app/robot.rb
@@ -1,8 +1,12 @@
 class Robot
+  DIRECTIONS = ['N', 'E', 'S', 'W'].freeze
   attr_accessor :position, :direction
   def initialize(position, direction)
     @position  = position
     @direction = direction
   end
-  
+
+  def turn_left!
+    @direction = DIRECTIONS[DIRECTIONS.index(direction) - 1]
+  end
 end
--- tests/robot_test.rb
+++ tests/robot_test.rb
@@ -5,12 +5,21 @@ require "./app/position"
 class TestRobot < Minitest::Test
   def test_basic_position
     pos = Position.new(1, 0)
-    assert_equal(pos, Robot.new(pos, 'N').position)
+    assert_equal(pos, new_robot(pos: pos).position)
   end
 
   def test_basic_direction
-    pos = Position.new(1, 0)
-    assert_equal('N', Robot.new(pos, 'N').direction)
+    assert_equal('N', new_robot.direction)
+  end
+
+  def test_turn_left_from_north
+    r = new_robot
+    r.turn_left!
+    assert_equal('W', r.direction)
+  end
+
+  def new_robot(pos: Position.new(1, 0), direction: 'N')
+    Robot.new(pos, direction)
   end
 
 end
